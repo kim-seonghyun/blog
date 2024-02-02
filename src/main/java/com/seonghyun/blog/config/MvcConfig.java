b4356aef6716d8f3dccd07a9f7390bc3d4086267
biggie <aruesin2@gmail.com>
2024-02-02 19:26:12 +0900

    :bento: book log view 구현

--- src/main/java/com/seonghyun/blog/config/MvcConfig.java
+++ src/main/java/com/seonghyun/blog/config/MvcConfig.java
@@ -1,2 +1,12 @@
-package com.seonghyun.blog.config;public class MvcConfig {
+package com.seonghyun.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+    }
 }
